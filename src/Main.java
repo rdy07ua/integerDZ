8029442f7e073558a8c87dd838cb933aecd8de79
rdy07ua <spartakadoma.net@gmail.com>
2023-05-31 21:05:13 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+// ЗАВДАННЯ 1
+// Результат простих математичних дій з заданними змінними.
+/*
+    int a = 10;
+    int b = 2;
+        System.out.println("Our numbers: " + a + " and " + b);
+        System.out.println("Addition");
+        System.out.println(a+b);
+        System.out.println("Subtraction");
+        System.out.println(a-b);
+        System.out.println("Multiplication");
+        System.out.println(a*b);
+        System.out.println("Division");
+        System.out.println(a/b);
+
+*/
+
+
+
+
+// ЗАВДАННЯ 2
+// Результати простих математичних дій з введеними користувачем числами в консолі. Використовується змінна float.
+/*
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your first number");
+        float firstnum = scanner.nextFloat();
+        System.out.println("Enter your second number");
+        float secondnum = scanner.nextFloat();
+        System.out.println("Result of Addition " + (firstnum + secondnum));
+        System.out.println("Result of Subtraction " + (firstnum - secondnum));
+        System.out.println("Result of Multiplication " + (firstnum * secondnum));
+        System.out.println("Result of Division " + (firstnum / secondnum));
+ */
+
+
+ /*
+
+
+
+
+        // ЗАВДАННЯ 3.
+        // Ну, щось типу калькулятора цілих чисел.
+
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Enter your first number (use just integer numbers please)");
+                int firstnum = scanner.nextInt();
+                System.out.println("Enter your second number (use just integer numbers please)");
+                int secondnum = scanner.nextInt();
+                System.out.println("Enter 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division ");
+                int sign = scanner.nextInt();
+        if (sign == 1) {
+            System.out.println("Your result is  " + (firstnum + secondnum));
+        }
+        else if (sign == 2) {
+        System.out.println("Your result is " + (firstnum- secondnum));
+        }
+        else if (sign == 3) {
+            System.out.println("Your result is " + (firstnum * secondnum));
+        }
+        else if (sign == 4) {
+            System.out.println("Your result is " + (firstnum / secondnum));
+        }
+
+        else {
+            System.out.println("You entered an incorrect number. Please try again! ");
+        }
+
+
+  */
+
+
+    }
+}
